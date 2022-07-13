b86b83a48000c141c35b54f4c72fc8367419725e
Alejo9601 <alejandro96jm@gmail.com>
2022-07-13 16:13:27 -0300

    Deleted useless comments

--- src/mongo.js
+++ src/mongo.js
@@ -10,19 +10,3 @@ mongoose
   .catch((error) => {
     console.error(error);
   });
-
-// const note = new Note({
-//   content: "MongoDB es increible, midu",
-//   date: new Date().toISOString(),
-//   important: true,
-// });
-
-// note
-//   .save()
-//   .then((result) => {
-//     console.log(result);
-//     mongoose.connection.close();
-//   })
-//   .catch((error) => {
-//     console.error("Something went wrong..." + error);
-//   });
